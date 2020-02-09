76a3f59184f96820c632036987fc8f679331ccbc
NAVEEN S R <56086391+nkpro2000sr@users.noreply.github.com>
2020-02-09 23:53:36 +0530

    ++customizability

--- Train_ImgClassfyr.py
+++ Train_ImgClassfyr.py
@@ -6,36 +6,76 @@ from tensorflow.keras.layers import Conv2D,MaxPooling2D
 
 import os, logging, json
 
-def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
-          earlystop_monitor = 'val_loss', # False for disable earlystop
-          n_cnn_layers = 4, batch_size = 32, show_graph = True) :
+def train(p_Tds, p_Vds, p_saveModel, shape,
+          epochs = 25, learning_rate = 0.001, #trade-off between time and accuracy
+          earlystop_monitor_and_mode = (-1,-1), callbacks = [],
+          IDG_kwargs = -1, # key word arguments for ImageDataGenerator
+          n_cnn_layers = 4, batch_size = 32,
+          get_filters = -2, get_kernel_size = -2, get_pool_size = -1,
+          save_json = True, show_graph = True) :
     """
     single function for traning model for all types of image classification
     eg: emotion detection, gender classification, etc.,
 
-    $tdd = path of the traning dataset directory
-    $vdd = path of the validation dataset directory
-    > dataset directory have subdirs with dirname= label
-    > and each subdir have pics of their respective lable
+    $p_Tds = path of the training dataset directory
+    $p_Vds = path of the validation dataset directory
+     dataset directory have subdirs with dirname= label
+     and each subdir have pics of their respective lable
 
-    $tmf = path to save the trained model
+    $p_saveModel = path to save the trained model
     $shape = pixel size of the image to be reshaped
+
+    $earlystop_monitor_and_mode to Stop training when a monitored quantity has stopped improving
+     = False => disable earlystop
+     = (monitor, mode) arguments of ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+    [0] = -1 => use default for monitor argument
+    [1] = -1 => use default for mode argument
+    $callbacks list of callables called while training model
+
+    $get_filters is a function to get filters for each Conv2D layers
+     = lambda i:filters_list[i-1] #i is index of each layers
+     = -2 => use recommended lambda i : batch_size*(2**(i-1)) #similar_to VGG16 architecture
+    $get_kernel_size is a function to get kernel_size for each Conv2D layers
+     = lambda i:kernel_size_list[i-1]
+     = -2 => use recommended lambda i : (3,3)
+    $get_pool_size is a function to get pool_size for each Con2D layers
+     = lambda i:pool_size_list[i-1]
+     = -1 => use default
+     = -2 => use recommended lambda i : (2,2)
+
+    $IDG_kwargs is a dict of {kew_word_arguments:values} in ImageDataGenerator
+     = dict => which is passed as keyword arguments
+     = -1 => use default (same as `IDG_kwargs=dict()`)
+     = -2 => use recommended
+
+    $save_json = True to save the jd (info_dict) as p_saveModel+'.json'
+     = path to save json file
+     = False to don't
+    $show_graph = True to show graph of ['loss','val_loss','accuracy','val_accuracy'] at training 
+     = False to don't
     """
     
     jd = dict()
 
-    train_data_dir = tdd; jd["train_data_dir"]= train_data_dir
+    training_data_dir = tdd; jd["training_data_dir"]= training_data_dir
     validation_data_dir = vdd; jd["validation_data_dir"]= validation_data_dir
     trained_model_file = tmf; jd["trained_model_file"]= trained_model_file
 
-    jd["labels"]= next(os.walk(train_data_dir))[1]
-    assert (next(os.walk(train_data_dir))[1] == next(os.walk(validation_data_dir))[1]),\
+    jd["labels"]= next(os.walk(training_data_dir))[1]
+    assert (jd["labels"] == next(os.walk(validation_data_dir))[1]),\
            "labels must be same in both traning dataset and validation dataset"
-    num_classes = len(next(os.walk(train_data_dir))[1]) # no. of subdirectories in train_data_dir
+    num_classes = len(jd["labels"]) # no. of subdirectories in training_data_dir
     jd["n_labels"]= num_classes
     image_shape = shape # (r,c,p) => Image with `r` x `c` pixcels and `p` values in each pixcel
     #                          p = 1 for grayscape and 3 for rgb or 4 for rgpa
     jd["image_shape"]= shape
+
+    jd["dataset"] = dict(training= defaultdict(list), validation= defaultdict(list))
+    for label in jd["labels"]:
+        for file in os.listdir(os.path.join(training_data_dir,label)):
+            jd["dataset"]["training"][label].append(os.path.join(training_data_dir,label,file))
+        for file in os.listdir(os.path.join(validation_data_dir,label)):
+            jd["dataset"]["validation"][label].append(os.path.join(validation_data_dir,label,file))
 
     # generating multiple images with different aspects from our train_data
 
@@ -46,7 +86,9 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
     else :
         mode = 'rgba'
 
-    train_datagen = ImageDataGenerator(
+    if IDG_kwargs == -1 : IDG_kwargs = dict()
+    elif IDG_kwargs == -2 :
+        IDG_kwargs = dict(
                         rescale=1./255,
                         rotation_range=30,
                         shear_range=0.3,
@@ -56,8 +98,10 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
                         horizontal_flip=True,
                         fill_mode='nearest')
 
+    train_datagen = ImageDataGenerator(rescale=1./255, **IDG_kwargs)
+
     train_generator = train_datagen.flow_from_directory(
-                        train_data_dir,
+                        training_data_dir,
                         color_mode=mode,
                         target_size=(image_shape[0],image_shape[1]),
                         batch_size=batch_size,
@@ -82,25 +126,30 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
 
     ## Convolutional Neural Network (CNN) Layers
 
-    get_filters_int = lambda n : batch_size*(2**(n+1)) # to get filter in each layer (iteration)
+    if get_filters == -2 : get_filters = lambda i : batch_size*(2**(i-1)) # to get filter in each layer (iteration)
+    if get_kernel_size == -2 : get_kernel_size = lambda i : (3,3) # to get kernel_size in each layer (iteration)
+    if get_pool_size == -2 : get_pool_size = lambda i : (2,2) # to get pool_size in each MaxPooling2D layer
 
-    model.add(Conv2D(batch_size,(3,3),padding='same',kernel_initializer='he_normal',input_shape= image_shape ))
+    # first layer i == 1
+    model.add(Conv2D(get_filters(1),get_kernel_size(1),padding='same',kernel_initializer='he_normal',input_shape= image_shape ))
     model.add(Activation('elu'))
     model.add(BatchNormalization())
-    model.add(Conv2D(batch_size,(3,3),padding='same',kernel_initializer='he_normal',input_shape= image_shape ))
+    model.add(Conv2D(get_filters(1),get_kernel_size(1),padding='same',kernel_initializer='he_normal',input_shape= image_shape ))
     model.add(Activation('elu'))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    if get_pool_size != -1 : model.add(MaxPooling2D(pool_size=get_pool_size(1)))
+    else : model.add(MaxPooling2D())
     model.add(Dropout(0.2))
 
     for i in range(number_of_cnn_layers-1):
-        model.add(Conv2D(get_filters_int(i),(3,3),padding='same',kernel_initializer='he_normal'))
+        model.add(Conv2D(get_filters(i+2),get_kernel_size(i+2),padding='same',kernel_initializer='he_normal'))
         model.add(Activation('elu'))
         model.add(BatchNormalization())
-        model.add(Conv2D(get_filters_int(i),(3,3),padding='same',kernel_initializer='he_normal'))
+        model.add(Conv2D(get_filters(i+2),get_kernel_size(i+2),padding='same',kernel_initializer='he_normal'))
         model.add(Activation('elu'))
         model.add(BatchNormalization())
-        model.add(MaxPooling2D(pool_size=(2,2)))
+        if get_pool_size != -1 : model.add(MaxPooling2D(pool_size=get_pool_size(i+2)))
+        else : model.add(MaxPooling2D())
         model.add(Dropout(0.2))
 
     ## Fully Connected Layer
@@ -129,28 +178,39 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
     from tensorflow.keras.optimizers import RMSprop,SGD,Adam
     from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
-    if earlystop_monitor :
+    fg_kwargs = dict()
+
+    if earlystop_monitor_and_mode :
+
+        es_kwargs = dict()
+        if earlystop_monitor_and_mode[0] != -1 : es_kwargs['monitor'] = earlystop_monitor_and_mode[0]
+        if earlystop_monitor_and_mode[1] != -1 : es_kwargs['mode'] = earlystop_monitor_and_mode[1]
 
         checkpoint = ModelCheckpoint(trained_model_file, # saving model_file
-                                monitor = earlystop_monitor,
-                                mode='min',
                                 save_best_only=True,
-                                verbose=1)
+                                verbose=1,
+                                **es_kwargs)
 
-        earlystop = EarlyStopping(monitor='val_loss',
-                            min_delta=0,
+        earlystop = EarlyStopping(min_delta=0,
                             patience=3,
                             verbose=1,
-                            restore_best_weights=True
-                            )
+                            restore_best_weights=True,
+                            **es_kwargs)
 
-        reduce_learning_rate = ReduceLROnPlateau(monitor='val_loss',
-                                factor=0.2,
+        reduce_learning_rate = ReduceLROnPlateau(factor=0.2,
                                 patience=3,
                                 verbose=1,
-                                min_delta=0.0001)
+                                min_delta=0.0001,
+                                **es_kwargs)
 
-        callbacks = [earlystop,checkpoint,reduce_learning_rate]
+        callbacks_ = [earlystop,checkpoint,reduce_learning_rate]
+        callbacks_.append(callbacks)
+
+        fg_kwargs["callbacks"] = callbacks_
+
+    if len(callbacks) :
+        if "callbacks" not in fg_kwargs.keys() :
+            fg_kwargs["callbacks"] = list(callbacks)
 
     model.compile(loss='categorical_crossentropy',
                 optimizer = Adam(lr=learning_rate),
@@ -158,27 +218,18 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
 
     ## training model
 
-    number_of_train_samples = len([j for _,_,i in os.walk(train_data_dir) for j in i])
+    number_of_train_samples = len([j for _,_,i in os.walk(training_data_dir) for j in i])
+    jd["number_of_train_samples"] = number_of_train_samples
     number_of_validation_samples = len([j for _,_,i in os.walk(validation_data_dir) for j in i])
+    jd["number_of_validation_samples"] = number_of_validation_samples
 
-    if earlystop_monitor :
-
-        history=model.fit_generator(
-                    train_generator,
-                    steps_per_epoch=number_of_train_samples//batch_size,
-                    epochs=epochs,
-                    callbacks=callbacks,
-                    validation_data=validation_generator,
-                    validation_steps=number_of_validation_samples)
-
-    else :
-
-        history=model.fit_generator(
+    history=model.fit_generator(
                     train_generator,
                     steps_per_epoch=number_of_train_samples//batch_size,
                     epochs=epochs,
                     validation_data=validation_generator,
-                    validation_steps=number_of_validation_samples)
+                    validation_steps=number_of_validation_samples,
+                    **fg_kwargs)
 
     model.save(trained_model_file)
 
@@ -187,8 +238,9 @@ def train(tdd, vdd, tmf, shape, epochs = 25, learning_rate = 0.001,
     history = history.history
     jd["history"]= history
 
-    with open(trained_model_file+".json",'w') as jd_file :
-        json.dump(jd, jd_file)
+    if save_json :
+        with open(save_json if type(save_json)==str else trained_model_file+".json",'w') as jd_file :
+            json.dump(jd, jd_file)
 
     import matplotlib.pyplot as plt
     plt.style.use("ggplot")
